e63e450f989f6d16c3a5ed1d3497546444b8df2d
B6001186 <atcharapornok@gmail.com>
2026-07-06 16:02:07 +0700

    edit file name

---
+++ exercise1.mongodb.js
@@ -0,0 +1,7 @@
+use("sample_mflix");
+
+//db.comments.find({_id: ObjectId("5a9427648b0beebeb69579f5")});
+
+//db.comments.find({email: "john_bishop@fakegmail.com"});
+
+db.comments.find({name: "John Bishop"});
