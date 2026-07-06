796c3df7ade2912ad9aad058082750f55e0fd977
B6001186 <atcharapornok@gmail.com>
2026-07-06 16:17:57 +0700

    add exercise3 file

---
+++ exercise3.mongodb.js
@@ -0,0 +1,7 @@
+use("sample_mflix");
+
+//db.theaters.find({"location.address.state": "AL"}).count();
+
+//db.theaters.find({"location.address.state": "La Quinta"}).count();
+
+db.theaters.findOne({"location.address.state": "La Quinta"});
