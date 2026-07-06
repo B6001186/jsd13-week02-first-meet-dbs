3ba833b929824adaf87221f31d11da504a83580c
B6001186 <atcharapornok@gmail.com>
2026-07-06 17:03:19 +0700

    edit excercise4 file

--- exercise4.mongodb.js
+++ exercise4.mongodb.js
@@ -1,9 +1,9 @@
 use("sample_mflix");
 
 //ค้นหาหนังที่มีคำว่า "American" ใน plot
-//db.movies.find({plot: {$regex: "American"}});
+//db.movies.find({plot: {$regex: "American"}}).count();
 
 //ค้นหาหนังที่มีคำลงท้ายว่า "street" ใน plot
-//db.movies.find({plot: {$regex: "street$"}});
+//db.movies.find({plot: {$regex: "street$"}}).count();
 
-db.movies.findOne({plot: {$regex: "street$"}});
+//db.movies.findOne({plot: {$regex: "street$"}});
